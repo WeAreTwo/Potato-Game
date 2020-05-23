c555c2f3111906ba5222ad7ae3cd7b38fd377ae7
Codrin-Mihail <kodrintxps@gmail.com>
2020-05-23 16:06:32 +0900

    added generic extension methods

---
+++ Assets/Scripts/Extensions/GenericExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PotatoGame
+{
+
+    public static class GenericExtension 
+    {
+        public static bool IsType<T>(this GameObject comparison) where T : class
+        {
+            if (comparison.GetComponent<T>() != null)
+                return true;
+            else
+                return false;
+        }        
+
+    }
+
+}
