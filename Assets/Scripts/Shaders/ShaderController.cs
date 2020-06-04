344c77302ddb9a5d93a997862e925ec3b356bb23
Codrin-Mihail <kodrintxps@gmail.com>
2020-06-05 00:17:38 +0900

    working on system for quickly editing scene pallette

--- Assets/Scripts/Shaders/ShaderController.cs
+++ Assets/Scripts/Shaders/ShaderController.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 
 namespace PotatoGame
 {
+    
+    [System.Serializable]
+    public class ColorProfile
+    {
+        public string name = "Primary";
+        public Color color = Color.cyan;
+        public Material mat;
 
+        public void ChangeColor()
+        {
+            mat.SetColor("_BaseColor", this.color);
+        }
+    }
+    
+    [ExecuteAlways]
     public class ShaderController : MonoBehaviour
     {
         //GLOBAL SHADER VARIABLES
+        [Header("GLOBAL PROPERTIES")]
         [SerializeField] protected Texture bluenoise;
 
-        void Start()
+        [Header("COLOR PALLETTE")]
+        [SerializeField] protected List<ColorProfile> colorProfiles = new List<ColorProfile>();
+        
+        void Start()    
         {
             Shader.SetGlobalTexture("_BlueNoiseMap", bluenoise);
             Shader.SetGlobalTexture("_DetailMap", bluenoise);
         }
 
-        void Update()
+        //when something changes in the inspector call this 
+        void OnValidate()
         {
+            foreach (var profile in colorProfiles)
+            {
+                if(profile.mat)
+                    profile.ChangeColor();
+            }
         }
     }
 }
