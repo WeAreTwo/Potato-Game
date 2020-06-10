f8af88fa6cc437ba2f9d720652c64140545269b4
Charles Doucet <charles.douc@gmail.com>
2020-06-10 00:58:56 -0400

    New Controls for the camera + Compatibility with controllers for all player controls

--- Assets/Scripts/Player/TopViewCameraController.cs
+++ Assets/Scripts/Player/TopViewCameraController.cs
@@ -11,6 +11,9 @@ namespace PotatoGame
         // public variables -------------------------
         [ProgressBar("m_zoomMin", "_mZoomMax", Height = 20)]
         public float m_currentZoom; // Current player's zoom
+        [ProgressBar("_mTiltMin", "_mTiltMax", Height = 20)]
+        public float m_currentTilt; // Current camera tilt 
+        [Space(10)]
         public float m_zoomSpeed = 300f; // Speed when zooming in/out
         public float m_rotateSpeed = 25f; // Rotation speed of the camera around the target
 
@@ -25,6 +28,8 @@ namespace PotatoGame
         private Camera _mCam; // Instance of the camera component
         private float _mZoomMin; // Minimum zoom of the camera
         private float _mZoomMax; // Maximum zoom of the camera
+        private float _mTiltMin; // Minimum incline value of the offset
+        private float _mTiltMax; // Maximum incline value of the offset
 
 
         // ------------------------------------------
@@ -39,6 +44,11 @@ namespace PotatoGame
             _mZoomMin = 10f;
             _mZoomMax = 35f;
             m_currentZoom = _mCam.fieldOfView;
+            
+            // Set tilt range and current tilt
+            _mTiltMin = 2.8f;
+            _mTiltMax = 20f;
+            m_currentTilt = offset.y;
         }
 
         // ------------------------------------------
@@ -54,6 +64,7 @@ namespace PotatoGame
         {
             // Look at the player
             RotateCam();
+            TiltCam();
             FollowTarget();
         }
 
@@ -79,10 +90,9 @@ namespace PotatoGame
         // Zoom in and out ---------------------------------------------------------
         private void Zoom()
         {
-            var step = m_zoomSpeed * Time.deltaTime;
-
             // Get the zoom directly from the mouse scroll input
-            m_currentZoom += Input.GetAxis("Mouse ScrollWheel") * -step;
+            var step = m_zoomSpeed * Time.deltaTime;
+            m_currentZoom += Input.GetAxis("Zoom") * -step * 2f;
 
             // Safe net it
             if (m_currentZoom < _mZoomMin)
@@ -96,14 +106,42 @@ namespace PotatoGame
             // Update current zoom
             _mCam.fieldOfView = smoothZoom;
         }
-
         
+        
+        // Tilt camera on the y axis -----------------------------------------------
+        private void TiltCam()
+        {
+            // Get the rotation axis
+            var step = m_currentTilt * Time.deltaTime;
+            m_currentTilt += Input.GetAxis("Tilt") * step * 5f;
+
+            // If player is holding the middle mouse
+            if (Input.GetAxisRaw("Middle") != 0)
+                m_currentTilt += Input.GetAxis("Mouse Y") * -step * 5f;
+
+            // Safe net it
+            if (m_currentTilt < _mTiltMin)
+                m_currentTilt = _mTiltMin;
+            else if (m_currentTilt > _mTiltMax)
+                m_currentTilt = _mTiltMax;
+
+            offset.y = m_currentTilt;
+        }
+        
+
         // Rotate the camera in a direction ----------------------------------------
         private void RotateCam()
         {
             // Get the rotation axis
             var rotationAxis = Input.GetAxis("Rotate");
             var step = rotationAxis * m_rotateSpeed;
+
+            // If player is holding the middle mouse
+            if (Input.GetAxisRaw("Middle") != 0)
+            {
+                rotationAxis = Input.GetAxis("Mouse X");
+                step = rotationAxis * m_rotateSpeed * 3f;
+            }
 
             // Rotate around the target while keeping the offset
             offset = Quaternion.AngleAxis(step, Vector3.up) * offset;
