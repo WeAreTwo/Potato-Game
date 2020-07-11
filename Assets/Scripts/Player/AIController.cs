324c2f9517c538fac1549f38d154c07139c9174f
Codrin-Mihail <kodrintxps@gmail.com>
2020-07-11 22:02:01 +0900

    click to move, need to polish/ will continue tomorrow

--- Assets/Scripts/Player/AIController.cs
+++ Assets/Scripts/Player/AIController.cs
@@ -8,12 +8,14 @@ namespace PotatoGame
 {
     public class AIController : MovementBase
     {
+        protected Vector3 mouseTest = Vector3.zero;
 
         protected override void Update()
         {
             base.Update();
             // Make the player able to move
             CheckInput();
+            MoveToMousePosition();
         }
         
         // Check user's input ------------------------------------------------------
@@ -30,12 +32,15 @@ namespace PotatoGame
             
             // Catch the inputs in a vector3
             // (make sure inputs makes sense with camera view)
+            float distToDest = Vector3.Distance(mouseTest, this.transform.position);
+            _mHeading = (mouseTest - this.transform.position).normalized;
+            _mHeading.y = 0;
             var move = _mHeading;
             move = Camera.main.transform.TransformDirection(move);
             move.y = 0f;
 
             // When we record input, move the controller
-            if (move != Vector3.zero)
+            if (distToDest >= 1.5f)
             {
                 move = Vector3.ClampMagnitude(move, 1);
                 _mController.Move(move * step);
@@ -55,6 +60,25 @@ namespace PotatoGame
             _mVelocity.y += m_gravityForce * Time.deltaTime;
             _mController.Move(_mVelocity * Time.deltaTime);
         }
-        
+
+        protected virtual void MoveToMousePosition()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 touchPos = Input.mousePosition;
+                Transform camTrans = Camera.main.transform;
+                float dist = Vector3.Dot(this.transform.position - camTrans.position, camTrans.forward);
+                touchPos.z = dist;
+                mouseTest = Camera.main.ScreenToWorldPoint(touchPos);
+
+            }
+        }
+
+        protected override void OnDrawGizmos()
+        {
+            base.OnDrawGizmos();
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(mouseTest, 0.5f);
+        }
     }
 }
