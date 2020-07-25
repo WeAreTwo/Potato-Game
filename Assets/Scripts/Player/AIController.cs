8cfa24117547003a0d384339e97ef99054af7527
Codrin-Mihail <kodrintxps@gmail.com>
2020-07-25 14:17:55 +0900

    added movement base to AI

--- Assets/Scripts/Player/AIController.cs
+++ Assets/Scripts/Player/AIController.cs
@@ -13,52 +13,28 @@ namespace PotatoGame
         protected override void Update()
         {
             base.Update();
-            // Make the player able to move
-            CheckInput();
+            // CheckInput();    
             MoveToMousePosition();
         }
         
         // Check user's input ------------------------------------------------------
-        private void CheckInput()
+        protected override void CheckInput()
         {
-            // Check if the player is grounded
-            _mIsGrounded = Physics.CheckSphere(_mGroundCheck.position, m_groundOffset, m_ground, QueryTriggerInteraction.Ignore);
-
             if (_mIsGrounded && _mVelocity.y < 0)
                 _mVelocity.y = 0f;
 
             // Step between each movement
-            var step = m_movementSpeed * Time.deltaTime;
+            _movementStep = m_movementSpeed * Time.deltaTime;
             
             // Catch the inputs in a vector3
             // (make sure inputs makes sense with camera view)
             float distToDest = Vector3.Distance(mouseTest, this.transform.position);
             _mHeading = (mouseTest - this.transform.position).normalized;
             _mHeading.y = 0;
-            var move = _mHeading;
-            move = Camera.main.transform.TransformDirection(move);
-            move.y = 0f;
-
-            // When we record input, move the controller
-            if (distToDest >= 1.5f)
-            {
-                move = Vector3.ClampMagnitude(move, 1);
-                _mController.Move(move * step);
-                _mLookRotation = Quaternion.LookRotation(move);
-                transform.rotation = Quaternion.Lerp(transform.rotation, _mLookRotation, m_rotationSpeed * Time.deltaTime);
-                
-                // Update the animator
-                _mAnim.SetBool("walking", true);
-            }
-            else
-            {
-                // Return to iddle state
-                _mAnim.SetBool("walking", false);
-            }
             
-            // Add gravity
-            _mVelocity.y += m_gravityForce * Time.deltaTime;
-            _mController.Move(_mVelocity * Time.deltaTime);
+            _movementDirection = _mHeading;
+            _movementDirection = Camera.main.transform.TransformDirection(_movementDirection);
+            _movementDirection.y = 0f;
         }
 
         protected virtual void MoveToMousePosition()
@@ -70,7 +46,7 @@ namespace PotatoGame
                 float dist = Vector3.Dot(this.transform.position - camTrans.position, camTrans.forward);
                 touchPos.z = dist;
                 mouseTest = Camera.main.ScreenToWorldPoint(touchPos);
-
+                mouseTest.y = 0;
             }
         }
 
