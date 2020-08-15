160e55491fde224406eda6d75e53904e265befb0
Codrin-Mihail <kodrintxps@gmail.com>
2020-08-15 22:30:38 +0900

    potato state labels

---
+++ Assets/Scripts/Editor/PotatoAIEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+namespace PotatoGame
+{
+
+    [CustomEditor(typeof(PotatoAI))]
+    public class PotatoAIEditor : Editor
+    {
+        void OnSceneGUI()
+        {
+            PotatoAI potato = (PotatoAI) target;
+
+            if (potato.Fsm.CurrentStateKey != null)
+            {
+                Handles.color = Color.red;
+                Handles.Label(potato.transform.position + Vector3.up * 2,
+                    potato.transform.position.ToString() + "\nState: " +
+                    potato.Fsm.CurrentStateKey.ToString());
+            }
+        }
+    }
+
+}
