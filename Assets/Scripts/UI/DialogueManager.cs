f257905a518788693325778a203ff67693378169
Charles Doucet <charles.douc@gmail.com>
2020-10-10 18:37:48 -0400

    Added dialogue space in ui + controller. Added a font for testing.

---
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    // public variables -------------------------
+    
+
+
+    // private variables ------------------------
+
+
+    // ------------------------------------------
+    // Start is called before update
+    // ------------------------------------------
+    void Start()
+    {
+        
+    }
+
+    // ------------------------------------------
+    // Update is called once per frame
+    // ------------------------------------------
+    void Update()
+    {
+        
+    }
+
+    // ------------------------------------------
+    // Methods
+    // ------------------------------------------
+}
