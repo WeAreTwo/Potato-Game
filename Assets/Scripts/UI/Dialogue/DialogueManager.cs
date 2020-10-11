b31d853093dc051fc05ed915f4dffc88e49f7797
Charles Doucet <charles.douc@gmail.com>
2020-10-11 18:08:28 -0400

    Integrated dialogue system + tutorial example

---
+++ Assets/Scripts/UI/Dialogue/DialogueManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Sirenix.OdinInspector;
+
+public class DialogueManager : MonoBehaviour
+{
+    // public variables -------------------------
+    public TextMeshProUGUI m_nameText;  // Display text on UI for the speaker name
+    public TextMeshProUGUI m_dialogueText;  // Display text on UI for the dialogue text
+    public Animator m_animator;  // Dialogue animator to trigger open and close animation
+    
+    // private variables ------------------------
+    private Queue<string> _mSentences;  // Holds the whole dialogue in a queue
+
+
+    // ------------------------------------------
+    // Start is called before update
+    // ------------------------------------------
+    void Start()
+    {
+        // Assigned an instance of queue of type string
+        _mSentences = new Queue<string>();
+        
+    }
+
+    // ------------------------------------------
+    // Update is called once per frame
+    // ------------------------------------------
+    void Update()
+    {
+        
+    }
+
+    // ------------------------------------------
+    // Methods
+    // ------------------------------------------
+    // Start a dialogue from a given set of sentences --------------------------
+    public void StartDialogue(Dialogue dialogue)
+    {
+        // Open animation and display name on UI
+        m_animator.SetBool("IsOpen", true);
+        m_nameText.text = dialogue.name;
+        
+        // Clear past dialogues
+        _mSentences.Clear();
+
+        // Enqueue each sentence from the new dialogue
+        foreach (var sentence in dialogue.sentences)
+        {
+            _mSentences.Enqueue(sentence);
+        }
+        
+        // Display the first sentence
+        DisplayNextSentence();
+    }
+
+    
+    // Display the next sentence of the dialogue -------------------------------
+    public void DisplayNextSentence()
+    {
+        // Make sure the dialogue is not over
+        if (_mSentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        // Print the next sentence on UI
+        var sentence = _mSentences.Dequeue();
+        m_dialogueText.text = sentence;
+    }
+
+    
+    // End the current dialogue ------------------------------------------------
+    public void EndDialogue()
+    {
+        // Close the dialogue 
+        m_animator.SetBool("IsOpen", false);
+    }
+}
---
+++ Assets/Scripts/UI/Dialogue/TutorialDialogue.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks.Unity.UnityGameObject;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class TutorialDialogue : MonoBehaviour
+{
+    // public variables -------------------------
+    [Title("Dialogue")]
+    public float m_delay;  // Delay (seconds) before tutorial dialogue is prompt
+    public float m_WaitForNextTip;  // Time to wait before accessing next tip
+    public Dialogue m_dialogue;  // The dialogue linked to this tutorial
+    
+    // private variables ------------------------
+    private float _mDelayTimer; // Instance of the timer for the delay
+    private bool _mTutorialStarted = false;  // Check if the tutorial have started
+    private DialogueManager m_dm;  // Instance of the dialogue manager
+    private float _mWaitTimer;  // Timer use for wait time between tips
+
+
+    // ------------------------------------------
+    // Start is called before update
+    // ------------------------------------------
+    void Start()
+    {
+        // Get component
+        m_dm = FindObjectOfType<DialogueManager>();
+        
+        // Set the timers
+        _mDelayTimer = m_delay;
+        _mWaitTimer = m_WaitForNextTip;
+
+    }
+
+    
+    // ------------------------------------------
+    // Update is called once per frame
+    // ------------------------------------------
+    void Update()
+    {
+        if (!_mTutorialStarted)
+            DelayCountDown();
+        else
+            CheckInput();
+    }
+
+    
+    // ------------------------------------------
+    // Methods
+    // ------------------------------------------
+    // Count down delay before starting tutorial -------------------------------
+    private void DelayCountDown()
+    {
+        // Count down
+        _mDelayTimer -= Time.deltaTime;
+
+        // Check if the delay is over
+        if (_mDelayTimer <= 0f)
+        {
+            StartTutorial();
+            _mTutorialStarted = true;
+        }
+    }
+    
+    
+    // Start the tutorial via de dm --------------------------------------------
+    private void StartTutorial()
+    {
+        // Start the tutorial dialogue on screen
+        m_dm.StartDialogue(m_dialogue);
+    }
+    
+
+    // Check input for this tutorial -------------------------------------------
+    private void CheckInput()
+    {
+        // Display the next sentence of the tutorial
+        if (Input.GetButton("Jump") && _mWaitTimer <= 0f)
+        {
+            // Reset wait time
+            _mWaitTimer = m_WaitForNextTip;
+            m_dm.DisplayNextSentence();
+        }
+
+        // Count down wait time
+        _mWaitTimer -= Time.deltaTime;
+    }
+}
