080cecb725953f538b98d9fa20364076f57bb3ca
Charles Doucet <charles.douc@gmail.com>
2020-11-01 17:05:32 -0500

    Poisson sampling added to cast planting spots on the island

---
+++ Assets/Scripts/Procedural/PlantingPointsGenerator.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class PlantingPointsGenerator : MonoBehaviour
+{
+    // public variables -------------------------
+    [Title("Points parameters")]
+    public float m_pointRadius;  // Radius of each point
+    public Vector2 m_regionSize;  // Region size where to spawn points (grid)
+    public int m_rejectionSamples = 30;  // Number of rejection per point allowed
+    [Title("Valid Planting Spot")] 
+    public LayerMask m_groundLayer;  // Layer mask to collide with (ground)
+    [ReadOnly] public List<Vector3> m_plantingSpots;  // Valid planting spot
+    [Title("Gizmos Settings")] 
+    public bool m_displayGizmos = true;  // Display all Gizmos in the scene view
+    [ShowIf("m_displayGizmos")] public float m_displayRadius = 1f;  // Radius of the points sphere in gizmos
+    [ShowIf("m_displayGizmos")] public float m_displayRaysHeight = 75f;  // Height of the lines (rays)
+    [ShowIf("m_displayGizmos")] public bool m_drawValidPoints = true;  // Display points that generate a valid spot
+    [ShowIf("m_displayGizmos")] public bool m_drawInvalidPoints = true;  // Display points that do not generate a valid spot
+    [ShowIf("m_displayGizmos")] public bool m_drawValidRays = true;  // Display rays that end with a valid spot
+    [ShowIf("m_displayGizmos")] public bool m_drawInvalidRays = true;  // Display rays that end with no valid spot
+    
+
+    // private variables ------------------------
+    private List<Vector2> _points;  // Capture all generated points
+    private Vector3[] _worldPoints;  // Points coordinate in 3d space
+    private List<int> _validPoints = new List<int>();  // Indexes of all valid generating points
+
+
+    // ------------------------------------------
+    // Start is called before update
+    // ------------------------------------------
+    void Start()
+    {
+        
+    }
+
+    // ------------------------------------------
+    // Update is called once per frame
+    // ------------------------------------------
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // ------------------------------------------
+    // Methods
+    // ------------------------------------------
+    
+    // Change when inspector changes -------------------------------------------
+    private void OnValidate()
+    {
+        // Clear all planting spots
+        m_plantingSpots.Clear();
+        _validPoints.Clear();
+
+        // Get points generated (poisson sampling)
+        _points = PoissonDiscSampling.GeneratePoints(m_pointRadius, m_regionSize, m_rejectionSamples);
+        
+        // Create array in relation of point in world space
+        _worldPoints = new Vector3[_points.Count];
+
+        // Convert each points to a 3D environment
+        for (int i = 0; i < _worldPoints.Length; i++)
+        {
+            _worldPoints[i] = new Vector3(_points[i].x - m_regionSize.x / 2, transform.position.y, 
+                _points[i].y - m_regionSize.y / 2);
+
+            
+            RaycastHit hit;
+            if (Physics.Raycast(_worldPoints[i], Vector3.down * 75f, out hit, Mathf.Infinity, m_groundLayer))
+            {
+                m_plantingSpots.Add(hit.point);
+                _validPoints.Add(i);
+            }
+        }
+    }
+    
+    
+    // Draw Gizmos into the scene ----------------------------------------------
+    private void OnDrawGizmos()
+    {
+        if (m_displayGizmos)
+        {
+            Vector3 gridCenter = transform.position;
+            Vector3 gridSize = new Vector3(m_regionSize.x, 0, m_regionSize.y);
+
+            // Draw the grid
+            Gizmos.DrawWireCube(gridCenter, gridSize);
+
+            // Make sure points are generated
+            if (_points != null)
+            {
+                for (int i = 0; i < _worldPoints.Length; i++)
+                {
+                    // Draw valid points
+                    if (_validPoints != null && _validPoints.Contains(i) && m_drawValidPoints)
+                    {
+                        // Capture the valid planting spot
+                        Gizmos.color = Color.white;
+                        Gizmos.DrawSphere(_worldPoints[i], m_displayRadius);
+
+                        if (m_drawValidRays)
+                            Gizmos.DrawRay(_worldPoints[i], Vector3.down * m_displayRaysHeight);
+                    }
+                    else if (m_drawInvalidPoints && _validPoints != null && !_validPoints.Contains(i))
+                    {
+                        Gizmos.color = Color.black;
+                        Gizmos.DrawSphere(_worldPoints[i], m_displayRadius);
+                        
+                        if (m_drawInvalidRays)
+                            Gizmos.DrawRay(_worldPoints[i], Vector3.down * m_displayRaysHeight);
+                    }
+                }
+
+                for (int i = 0; i < m_plantingSpots.Count; i++)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawSphere(m_plantingSpots[i], m_displayRadius);
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Procedural/PoissonDiscSampling.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public static class PoissonDiscSampling
+{
+    // Generate points on a 2d grid --------------------------------------------
+    public static List<Vector2> GeneratePoints(float radius, Vector2 sampleRegionSize, int numSampleBeforeRejection = 30)
+    {
+        // Use pythagoras theorem to calculate the side of a grid cell
+        // Since we know the radius of a disc should fit in a cell : s = r / Sqr(2)
+        float cellSize = radius / Mathf.Sqrt(2);
+        
+        /*
+         * -------------------------------------------------
+         * This grid will tell for each cell, what the index of the point in the points list is,
+         * that lies in that cell. ( 0 = no point in that cell, 1 = point in the cell)
+         *
+         * When we add a new point, we also add a new spawn point,
+         * we would want to add future points around that spawn point 
+         * -------------------------------------------------
+         */
+        int[,] grid = new int[Mathf.CeilToInt(sampleRegionSize.x / cellSize), Mathf.CeilToInt(sampleRegionSize.y / cellSize)];
+        List<Vector2> points = new List<Vector2>();
+        List<Vector2> spawnPoints = new List<Vector2>();
+        
+        // Listening to add a starting point as our spawn point (in the middle of the sample)
+        spawnPoints.Add(sampleRegionSize / 2);
+        
+        // While the spawn points list is not empty
+        while (spawnPoints.Count > 0)
+        {
+            // Pick a random spawn point
+            int spawnIndex = Random.Range(0, spawnPoints.Count);
+            Vector2 spawnCentre = spawnPoints[spawnIndex];
+
+            bool candidateAccepted = false;
+            
+            for (int i = 0; i < numSampleBeforeRejection; i++)
+            {
+                // Pick a random direction
+                float angle = Random.value * Mathf.PI * 2;
+                Vector2 direction = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+                
+                // Get a candidate from the middle and new direction
+                // Double the radius to make sure to be outside the middle point range
+                Vector2 candidate = spawnCentre + direction * Random.Range(radius, 2 * radius);
+                
+                // Determine if we accept the candidate or not
+                if (IsValid(candidate, sampleRegionSize, cellSize, radius, points, grid))
+                {
+                    // If valid, we add the candidate to the points list and as a new spawn point
+                    points.Add(candidate);
+                    spawnPoints.Add(candidate);
+                    
+                    // Record which cell it ends up in
+                    grid[(int) (candidate.x / cellSize), (int) (candidate.y / cellSize)] = points.Count;
+                    
+                    // Candidate accepted
+                    candidateAccepted = true;
+                    break;
+                }
+            }
+            
+            // If no candidate was recorded
+            if (!candidateAccepted)
+            {
+                // Get rid of the point
+                spawnPoints.RemoveAt(spawnIndex);
+            }
+        }
+        // Return the point list
+        return points;
+    }
+    
+    
+    
+    // Check if a candidate point is valid or not ------------------------------
+    static bool IsValid(Vector2 candidate, Vector2 sampleRegionSize, float cellSize, float radius, List<Vector2> points, int[,] grid)
+    {
+        // Does the candidate lies within the sample region?
+        if (candidate.x >= 0 && candidate.x < sampleRegionSize.x &&
+            candidate.y >= 0 && candidate.y < sampleRegionSize.y)
+        {
+            // In which cell the candidate does lie in? (to search surrounding cells)
+            int cellX = (int) (candidate.x / cellSize);
+            int cellY = (int) (candidate.y / cellSize);
+            
+            // Search the 5x5 block around the cell (2 on the left, and 2 on the right)
+            // Min Max to make sure it doesn't go out of bounds of our grid
+            int searchStartX = Mathf.Max(0,cellX - 2);
+            int searchEndX = Mathf.Min(cellX + 2, grid.GetLength(0) - 1);
+            int searchStartY = Mathf.Max(0,cellY - 2);
+            int searchEndY = Mathf.Min(cellY + 2, grid.GetLength(1) - 1);
+
+            for (int x = searchStartX; x <= searchEndX; x++)
+            {
+                for (int y = searchStartY; y <= searchEndY; y++)
+                {
+                    // Catch a point index
+                    int pointIndex = grid[x, y] - 1;
+                    
+                    // If the point index is equal to -1, that means, no point in that cell
+                    if (pointIndex != -1)
+                    {
+                        // I use sqr for optimisation (less costly for magnitude)
+                        // That's why radius is squared in the check
+                        float sqrDistance = (candidate - points[pointIndex]).sqrMagnitude;
+                        
+                        // Is it less than the radius?
+                        if (sqrDistance < radius * radius)
+                        {
+                            // Candidate is too close to the point, not valid
+                            return false;
+                        }
+                    }
+                }
+            }
+            // Candidate is valid
+            return true;
+        }
+        
+        // Not valid
+        return false;
+    }
+}
